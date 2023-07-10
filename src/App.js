9398fa3a03855938be0ae2c7168c45c111e98d13
Marisa <lermamarisa@gmail.com>
2023-07-10 09:57:57 -0700

    setting up letter board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+import Letter from './components/Letter';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <nav>
+        <h1>WORDLE</h1>
+      </nav>
+      <Board />
+      <Keyboard />  
+      <Letter />
     </div>
   );
 }
